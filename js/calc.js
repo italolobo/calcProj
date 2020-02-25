5b3b1680d957d60143b60f4e71b5cfec7d38d7a0
italolobo <italoantuneslobo@gmail.com>
2020-02-25 18:32:11 +0000

    onclick

--- js/calc.js
+++ js/calc.js
@@ -1,11 +1,21 @@
-
 let a = [];
-let btn1 = document.getElementById("1");
-console.log(btn1);
+
+// let btn1 = document.getElementById("1");
+// btn1.addEventListener("click", function () {
+//     console.log("click");
+// })
+
+// let bb = document.addEventListener("click", function(){
+//     console.log(bb.value);
+// })
+
+function btnClicked(button) {
+  console.log(button);
+}
 
 // do {
-//   switch(/*ler){
-//       case 1: 
+//   switch(){
+//       case 1:
 //     }
 
 // } while (true);
